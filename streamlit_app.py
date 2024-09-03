4f21b49f15d7d19d8a1a7ec8885158b0ae61f0fa
MichelleJ-pro <michelle.johannknecht@ifb.ey.com>
2024-09-03 19:51:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,8 @@ if ingridients_list:
     
     import requests
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    st.text(fruityvice_response.json())
+    #st.text(fruityvice_response.json())
+    fv_df = st.dataframe(data=fruityvice_response.jason(), use_container_width=True)
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
